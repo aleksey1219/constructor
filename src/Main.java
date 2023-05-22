edbab48c4dff0b337bcb876f9452e14b834ec749
DNS <glotov-aleks@mail.ru>
2023-05-22 18:27:35 +0500

    task

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,66 @@
 public class Main {
+    public class Book {
+        private String name;
+        private String author;
+        private int age;
+
+        public Book(String name, String author, int age) {
+            this.Name = name;
+            this.author = author;
+            this.age = age;
+        }
+
+        public String getName() {
+            return this.name;
+        }
+
+        public String getAuthor() {
+            return this.author;
+        }
+
+        public int getAge() {
+            return this.age;
+        }
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public void setAuthor(String author) {
+            this.author = author;
+        }
+
+        public void setAge(int age) {
+            this.age = age;
+        }
+    }
+
+    public class Author {
+        private String nameAuthor;
+        private String surname;
+
+        public Author(String nameAuthor, String surname) {
+            this.nameAuthor = nameAuthor;
+            this.surname = surname;
+        }
+        public String getNameAuthor() {
+            return this.nameAuthor;
+        }
+        public String getSurname() {
+            return this.surname;
+        }
+
+        public void setNameAuthor(String NameAuthor) {
+            this.nameAuthor = NameAuthor;
+        }
+
+        public void setSurname(String surname) {
+            this.surname = surname;
+        }
+    }
+
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
     }
 
 }
+
