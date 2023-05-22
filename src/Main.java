209149a84b2b844ff83d93369c53f0228f496d2b
DNS <glotov-aleks@mail.ru>
2023-05-22 19:11:25 +0500

    task1.2

--- src/Main.java
+++ src/Main.java
@@ -1,66 +1,59 @@
 public class Main {
-    public class Book {
-        private String name;
-        private String author;
-        private int age;
-
-        public Book(String name, String author, int age) {
-            this.Name = name;
-            this.author = author;
-            this.age = age;
-        }
-
-        public String getName() {
-            return this.name;
-        }
-
-        public String getAuthor() {
-            return this.author;
-        }
-
-        public int getAge() {
-            return this.age;
-        }
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public void setAuthor(String author) {
-            this.author = author;
-        }
-
-        public void setAge(int age) {
-            this.age = age;
-        }
-    }
-
-    public class Author {
-        private String nameAuthor;
-        private String surname;
-
-        public Author(String nameAuthor, String surname) {
-            this.nameAuthor = nameAuthor;
-            this.surname = surname;
-        }
-        public String getNameAuthor() {
-            return this.nameAuthor;
-        }
-        public String getSurname() {
-            return this.surname;
-        }
-
-        public void setNameAuthor(String NameAuthor) {
-            this.nameAuthor = NameAuthor;
-        }
-
-        public void setSurname(String surname) {
-            this.surname = surname;
-        }
-    }
-
-
     public static void main(String[] args) {
-    }
 
+
+         class Book {
+            private String name;
+            private String author;
+            private int age;
+
+            public Book(String name, String author, int age) {
+                this.name = name;
+                this.author = author;
+                this.age = age;
+            }
+
+            public String getName() {
+                return this.name;
+            }
+
+            public String getAuthor() {
+                return this.author;
+            }
+
+            public int getAge() {
+                return this.age;
+            }
+
+            public void setAge(int age) {
+                this.age = age;
+            }
+        }
+
+         class Author {
+            private String nameAuthor;
+            private String surname;
+
+            public Author(String nameAuthor, String surname) {
+                this.nameAuthor = nameAuthor;
+                this.surname = surname;
+            }
+
+            public String getNameAuthor() {
+                return this.nameAuthor;
+            }
+
+            public String getSurname() {
+                return this.surname;
+            }
+        }
+
+            Book book = new Book("Капитанская дочка ", "Пушкин ", 13);
+            Author author = new Author("Александр", "Пушкин");
+            Book book1 = new Book("Му-му ", "Тургенев ", 15);
+            Author author1 = new Author("Иван ", "Тургенев ");
+            book1.setAge(20);
+            System.out.println(book.getName() + book.getAuthor() + book.getAge());
+    }
 }
 
