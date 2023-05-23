f685bfcc5da4b186b3e76974f2c90ca4a3469166
DNS <glotov-aleks@mail.ru>
2023-05-23 18:53:38 +0500

    task2.0

--- src/Main.java
+++ src/Main.java
@@ -1,59 +1,58 @@
 public class Main {
     public static void main(String[] args) {
-
-
-         class Book {
-            private String name;
-            private String author;
-            private int age;
-
-            public Book(String name, String author, int age) {
-                this.name = name;
-                this.author = author;
-                this.age = age;
-            }
-
-            public String getName() {
-                return this.name;
-            }
-
-            public String getAuthor() {
-                return this.author;
-            }
-
-            public int getAge() {
-                return this.age;
-            }
-
-            public void setAge(int age) {
-                this.age = age;
-            }
-        }
-
-         class Author {
-            private String nameAuthor;
-            private String surname;
-
-            public Author(String nameAuthor, String surname) {
-                this.nameAuthor = nameAuthor;
-                this.surname = surname;
-            }
-
-            public String getNameAuthor() {
-                return this.nameAuthor;
-            }
-
-            public String getSurname() {
-                return this.surname;
-            }
-        }
-
-            Book book = new Book("Капитанская дочка ", "Пушкин ", 13);
-            Author author = new Author("Александр", "Пушкин");
-            Book book1 = new Book("Му-му ", "Тургенев ", 15);
-            Author author1 = new Author("Иван ", "Тургенев ");
-            book1.setAge(20);
-            System.out.println(book.getName() + book.getAuthor() + book.getAge());
+        Book book = new Book("Александр ", "Пушкин ", " Капитанская дочка ", 1700);
+        System.out.println("book.getNameAuthor() = " + book.getNameAuthor());
+        Author author = new Author("Александр", "Пушкин");
+        Book book1 = new Book("Иван ", "Тургенев", "Му-му", 1289);
+        Author author1 = new Author("Иван", "Тургенев");
+        book1.setAge(1245);
+        System.out.println("book1.getAge() = " + book1.getAge());
     }
 }
+
+class Book extends Author {
+    private String name;
+
+    private int age;
+
+
+    public Book(String nameAuthor, String surname, String name, int age) {
+        super(nameAuthor, surname);
+        this.age = age;
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
+
+class Author {
+    private String nameAuthor;
+    private String surname;
+
+    public Author(String nameAuthor, String surname) {
+        this.nameAuthor = nameAuthor;
+        this.surname = surname;
+    }
+
+    public String getNameAuthor() {
+        return this.nameAuthor;
+    }
+
+    public String getSurname() {
+        return this.surname;
+    }
+}
+
+
 
